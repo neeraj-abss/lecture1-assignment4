735dc4318e789a3bf38c683c186be1555c131f36
MishraN <mishran@advisory.com>
2016-04-18 11:30:36 +0530

    assignment4

---
+++ assignment4.java
@@ -0,0 +1,14 @@
+class assignment4 {
+
+	public static void main(String args[]){
+
+		int x=5;
+		int y=10;
+
+		int case1=x+y*2;
+		int case2=x-y*2;
+		int case3=(x+y)*2;
+		int case4=y%x;
+		System.out.println(" case1="+case1+" case2="+case2+ " case3="+case3+" case4 = "+case4);
+	}
+}
